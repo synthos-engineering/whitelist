7f34f85de8fa011efefa307b2f35f0c4f729b3cc
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2025-05-20 10:40:26 +0800

    fix: update landing page heading

--- app/page.tsx
+++ app/page.tsx
@@ -164,8 +164,7 @@ export default function LandingPage() {
             SYNTHOS WAITLIST
           </p>
           <h1 className="text-3xl md:text-4xl font-bold mb-2 text-gray-900">
-            DeFi Made Personal, <br />
-            <span className="text-indigo-600">At Your Fingertips</span>
+            DeFi Made Personal <br />
           </h1>
           <p className="text-gray-600 text-base max-w-md mx-auto mt-4">
             Thousands of curated APY plans tailored to your unique preferences.
